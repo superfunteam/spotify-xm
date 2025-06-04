e628e776b103fa3593ec797263f9f73e87b99551
Clark Wimberly <clark@clarklab.net>
2025-06-04 15:28:04 -0500

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        spotify: '#00FF19',
-      },
-    },
+    extend: {},
   },
   plugins: [
     function({ addUtilities }) {
